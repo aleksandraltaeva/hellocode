2f0502ecb00c8b037358820cc7661d18e206fe68
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-03-19 21:43:40 +0300

    add changes Example012_Methods02 (max)

--- example012_Methods02/Program.cs
+++ example012_Methods02/Program.cs
@@ -13,15 +13,15 @@ void selectionSort(int[] array)
 {
     for (int i = 0; i < array.Length - 1; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
 
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] > array[maxPosition]) maxPosition = j;
         }
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     }
 }
 
